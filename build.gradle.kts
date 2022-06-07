45d6d1cd66df7df793d5ba430a9e7c01891066cc
Steve Haslam <araqnid@gmail.com>
2022-06-07 19:29:47 +0100

    Kotlin 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("js") version "1.6.20"
-    kotlin("plugin.serialization") version "1.6.20" apply false
+    kotlin("js") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21" apply false
     id("package-github-action")
 }
 
