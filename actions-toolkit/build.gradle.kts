de02c2b674e71a426c18a95c68e27da9092a4686
Steve Haslam <araqnid@gmail.com>
2022-06-07 19:36:37 +0100

    Specify coroutines 1.6.1 throughout

--- actions-toolkit/build.gradle.kts
+++ actions-toolkit/build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
     api(kotlin("stdlib-js"))
     implementation(npm("@actions/core", "latest"))
     implementation(npm("@actions/cache", "latest"))
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.0"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.1"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
     testImplementation(kotlin("test-js"))
     testImplementation("org.araqnid.kotlin.assert-that:assert-that:0.1.1")
